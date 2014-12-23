022a179579f87ed3f74f5304f6434fd419f39f80
Radu Helstern <radu.helstern@gmail.com>
2014-12-23 00:37:35 +0000

    add separate shell script for package source synchronizing

--- vagrantfile.d/provision.001.shell.rb
+++ vagrantfile.d/provision.001.shell.rb
@@ -1,7 +1,15 @@
 Vagrant.configure(VAGRANTFILE_API_VERSION) { |config|
 
   config.vm.provision :shell do |shell|
-    shell.path = "#{MACHINE_PROVISIONER_PATH}/shell/puppet_install.sh"
+    shell.path = "#{MACHINE_PROVISIONER_PATH}/shell/0001.sync_packages.sh"
+  end
+
+}
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) { |config|
+
+  config.vm.provision :shell do |shell|
+    shell.path = "#{MACHINE_PROVISIONER_PATH}/shell/0002.puppet_install.sh"
     shell.args = ['--puppet_source /puppet']
   end
 
